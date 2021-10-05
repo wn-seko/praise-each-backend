0e4013695460e861a24bc8caaa081bf748eeaa23
wn-seko <wn.seko@gmail.com>
2021-10-06 01:05:27 +0900

    Dockerを軽量化

--- deploy-script/packaging.app.sh
+++ deploy-script/packaging.app.sh
@@ -4,10 +4,10 @@ set -eu
 SCRIPT_DIR=$(cd $(dirname "${0}") && pwd)
 REVISION=$(git show -s --format=%H | cut -c 1-7)
 
-LATEST_TAG="${REPOSITORY}:latest"
-REVISION_TAG="${REPOSITORY}:${REVISION}"
+LATEST_TAG="${APP_REPOSITORY}:latest"
+REVISION_TAG="${APP_REPOSITORY}:${REVISION}"
 
-docker build -t ${LATEST_TAG} -f Dockerfile.prod .
+docker build -t ${LATEST_TAG} -f Dockerfile.app.prod .
 docker tag ${LATEST_TAG} ${REVISION_TAG}
 docker push ${LATEST_TAG}
 docker rmi ${LATEST_TAG} ${REVISION_TAG}
