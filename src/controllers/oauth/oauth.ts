4997a84fa009525966153f7aa9de9638cddc5244
wn-seko <wn.seko@gmail.com>
2021-11-03 04:56:00 +0900

    add update_icon and linkage API

--- src/controllers/oauth/oauth.ts
+++ src/controllers/oauth/oauth.ts
@@ -1,4 +1,4 @@
-import Koa from 'koa'
+import { CustomContext } from '~/middlewares/context'
 import { OAuthService } from '~/services/oauth'
 
 export class OAuthController {
@@ -8,20 +8,74 @@ export class OAuthController {
     this.oauthService = oauthService
   }
 
-  public async getLoginUrls(ctx: Koa.Context): Promise<void> {
-    const urls = await this.oauthService.getLoginUrls()
+  public async getLoginUrls(ctx: CustomContext): Promise<void> {
+    const urls = await this.oauthService.getOAuthLinks('login')
     ctx.body = urls
   }
 
-  public async githubLogin(ctx: Koa.Context): Promise<void> {
+  public async getUpdateIconUrls(ctx: CustomContext): Promise<void> {
+    const urls = await this.oauthService.getOAuthLinks('update_icon')
+    ctx.body = urls
+  }
+
+  public async getLinkageUrls(ctx: CustomContext): Promise<void> {
+    const urls = await this.oauthService.getOAuthLinks('linkage')
+    ctx.body = urls
+  }
+
+  public async githubLogin(ctx: CustomContext): Promise<void> {
     const { code } = ctx.request.body
-    const { token } = await this.oauthService.githubLogin(code || '')
+    const { token } = await this.oauthService.login('github', code || '')
     ctx.body = { token }
   }
 
-  public async googleLogin(ctx: Koa.Context): Promise<void> {
+  public async githubUpdateIcon(ctx: CustomContext): Promise<void> {
+    const userId = ctx.authUserId
     const { code } = ctx.request.body
-    const { token } = await this.oauthService.googleLogin(code || '')
+    const { token } = await this.oauthService.updateIcon(
+      userId,
+      'github',
+      code || '',
+    )
+    ctx.body = { token }
+  }
+
+  public async githubLinkage(ctx: CustomContext): Promise<void> {
+    const userId = ctx.authUserId
+    const { code } = ctx.request.body
+    const { token } = await this.oauthService.linkage(
+      userId,
+      'github',
+      code || '',
+    )
+    ctx.body = { token }
+  }
+
+  public async googleLogin(ctx: CustomContext): Promise<void> {
+    const { code } = ctx.request.body
+    const { token } = await this.oauthService.login('google', code || '')
+    ctx.body = { token }
+  }
+
+  public async googleUpdateIcon(ctx: CustomContext): Promise<void> {
+    const userId = ctx.authUserId
+    const { code } = ctx.request.body
+    const { token } = await this.oauthService.updateIcon(
+      userId,
+      'google',
+      code || '',
+    )
+    ctx.body = { token }
+  }
+
+  public async googleLinkage(ctx: CustomContext): Promise<void> {
+    const userId = ctx.authUserId
+    const { code } = ctx.request.body
+    const { token } = await this.oauthService.linkage(
+      userId,
+      'google',
+      code || '',
+    )
     ctx.body = { token }
   }
 }
--- src/services/oauth/oauth.ts
+++ src/services/oauth/oauth.ts
@@ -1,11 +1,19 @@
-import jwt from 'jsonwebtoken'
-import request from 'request'
 import { User } from '~/domains/entities/user'
 import { UserCredential } from '~/domains/entities/userCredential'
 import { UserRepository } from '~/domains/repositories/user'
 import { UserCredentialRepository } from '~/domains/repositories/userCredential'
 import { env } from '~/env'
 import { ApplicationError, errorCode } from '~/services/errors'
+import { getJWT } from '~/utils/jwt'
+import { get, post } from '~/utils/request'
+
+type OAuthType = 'github' | 'google'
+
+type OAuthState = 'login' | 'update_icon' | 'linkage'
+
+interface AccessTokenResponse {
+  access_token: string
+}
 
 interface GithubProfile {
   id: string
@@ -25,6 +33,16 @@ interface GoogleProfile {
   }
 }
 
+interface Profile {
+  id: string
+  name: string
+  icon: string
+}
+
+interface TokenResponse {
+  token: string
+}
+
 export class OAuthService {
   private readonly userRepository: UserRepository
   private readonly userCredentialRepository: UserCredentialRepository
@@ -37,29 +55,36 @@ export class OAuthService {
     this.userCredentialRepository = userCredentialRepository
   }
 
+  private async checkExistsUser(userId: string): Promise<User> {
+    const user = await this.userRepository.getById(userId)
+
+    if (!user) {
+      throw new ApplicationError(errorCode.NOT_FOUND, 'User is not found.', {
+        id: userId,
+      })
+    }
+
+    return user
+  }
+
   private async findOrCreate(
-    oauthId: string,
-    oauthType: 'github' | 'google',
-    name: string,
-    icon: string,
+    type: 'github' | 'google',
+    profile: Profile,
   ): Promise<User> {
+    const name = profile.name || 'Anonymous'
+    const icon = profile.icon || ''
     const sanitizedName = name.replace(/\s/g, '_')
-    const user = await this.userRepository.getByOAuth(oauthId, oauthType)
+    const user = await this.userRepository.getByOAuth(profile.id, type)
 
     if (user) {
-      if (user.name !== sanitizedName || user.icon !== icon) {
-        user.update({ name: sanitizedName, icon })
-        await this.userRepository.update(user)
-      }
-
       return user
     }
 
     const newUser = new User({ name: sanitizedName, icon })
     const newUserCredential = new UserCredential({
       userId: newUser.id,
-      oauthId,
-      oauthType,
+      oauthId: profile.id,
+      oauthType: type,
     })
 
     const newUserResult = await this.userRepository.create(newUser)
@@ -68,67 +93,101 @@ export class OAuthService {
     return newUserResult
   }
 
-  private getJWT(user: User): string {
-    const token = jwt.sign(user.toJSON(), env.APPLICATION_SECRET, {
-      expiresIn: '1h',
-    })
-    return token
+  private createAuthenticationError(
+    code?: string,
+    error?: Error,
+  ): ApplicationError {
+    const parameter = code ? { code } : {}
+
+    return new ApplicationError(
+      errorCode.AUTHENTICATION_ERROR,
+      'OAuth authentication failure.',
+      parameter,
+      error ? error : undefined,
+    )
   }
 
-  private async post<T extends {}>(
-    uri: string,
-    data: Record<string, string> = {},
-    headers: Record<string, string> = {},
-  ): Promise<{ response: request.Response; body: T }> {
-    return new Promise((resolve, reject) => {
-      const defaultHeaders = {
-        'Content-type': 'application/json',
-        'User-Agent': 'node',
-      }
-      const options = {
-        uri,
-        headers: { ...defaultHeaders, ...headers },
-        json: data,
-      }
+  private async oauthGithub(code: string): Promise<Profile> {
+    try {
+      // Fetch Github Access Token
+      const { body: accessTokenResult } = await post<AccessTokenResponse>(
+        `https://${env.OAUTH_GITHUB_DOMAIN}/login/oauth/access_token`,
+        {
+          client_id: env.OAUTH_GITHUB_CLIENT_ID,
+          client_secret: env.OAUTH_GITHUB_CLIENT_SECRET,
+          code,
+        },
+      )
 
-      request.post(options, (error, response, body) => {
-        if (error) {
-          return reject(error)
-        }
+      const { body: profile } = await get<GithubProfile>(
+        'https://api.github.com/user',
+        {},
+        { Authorization: `Bearer ${accessTokenResult.access_token}` },
+      )
 
-        resolve({ response, body })
-      })
-    })
+      return {
+        id: profile.id,
+        name: profile.login,
+        icon: profile.avatar_url,
+      }
+    } catch (error) {
+      console.error(error)
+      throw this.createAuthenticationError(code, error as Error)
+    }
   }
 
-  private async get<T extends {}>(
-    uri: string,
-    parameter: Record<string, string> = {},
-    headers: Record<string, string> = {},
-  ): Promise<{ response: request.Response; body: T }> {
-    return new Promise((resolve, reject) => {
-      const defaultHeaders = {
-        'Content-type': 'application/json',
-        'User-Agent': 'node',
-      }
-      const options = {
-        uri,
-        headers: { ...defaultHeaders, ...headers },
-        parameter,
-        json: true,
+  private async oauthGoogle(code: string): Promise<Profile> {
+    try {
+      // Fetch Google Access Token
+      const { body: accessTokenResult } = await post<AccessTokenResponse>(
+        `https://accounts.google.com/o/oauth2/token`,
+        {
+          client_id: env.OAUTH_GOOGLE_CLIENT_ID,
+          client_secret: env.OAUTH_GOOGLE_CLIENT_SECRET,
+          redirect_uri: env.OAUTH_GOOGLE_CALLBACK_URL,
+          grant_type: 'authorization_code',
+          code,
+        },
+      )
+
+      const { body: result } = await get<GoogleProfile>(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessTokenResult.access_token}`,
+        {},
+      )
+
+      if (result.error) {
+        throw new ApplicationError(
+          errorCode.AUTHENTICATION_ERROR,
+          'Google authentication failure.',
+        )
       }
 
-      request.get(options, (error, response, body) => {
-        if (error) {
-          return reject(error)
-        }
-
-        resolve({ response, body })
-      })
-    })
+      return {
+        id: result.id,
+        name: result.name,
+        icon: result.picture,
+      }
+    } catch (error) {
+      console.error(error)
+      throw this.createAuthenticationError(code, error as Error)
+    }
   }
 
-  public async getLoginUrls(): Promise<{ [name: string]: string }> {
+  private async fetchOAuthProfile(
+    type: OAuthType,
+    code: string,
+  ): Promise<Profile> {
+    switch (type) {
+      case 'github':
+        return this.oauthGithub(code)
+      case 'google':
+        return this.oauthGoogle(code)
+    }
+  }
+
+  public async getOAuthLinks(
+    state: OAuthState,
+  ): Promise<{ [name: string]: string }> {
     const urls: { [name: string]: string } = {}
 
     if (
@@ -137,7 +196,7 @@ export class OAuthService {
       env.OAUTH_GITHUB_CLIENT_SECRET &&
       env.OAUTH_GITHUB_CALLBACK_URL
     ) {
-      urls.github = `https://${env.OAUTH_GITHUB_DOMAIN}/login/oauth/authorize?client_id=${env.OAUTH_GITHUB_CLIENT_ID}&scope=public_user`
+      urls.github = `https://${env.OAUTH_GITHUB_DOMAIN}/login/oauth/authorize?client_id=${env.OAUTH_GITHUB_CLIENT_ID}&scope=public_user&state=${state}`
     }
 
     if (
@@ -145,107 +204,66 @@ export class OAuthService {
       env.OAUTH_GOOGLE_CLIENT_SECRET &&
       env.OAUTH_GOOGLE_CALLBACK_URL
     ) {
-      urls.google = `https://accounts.google.com/o/oauth2/auth?&client_id=${env.OAUTH_GOOGLE_CLIENT_ID}&redirect_uri=${env.OAUTH_GOOGLE_CALLBACK_URL}&scope=https://www.googleapis.com/auth/userinfo.profile&response_type=code`
+      urls.google = `https://accounts.google.com/o/oauth2/auth?&client_id=${env.OAUTH_GOOGLE_CLIENT_ID}&redirect_uri=${env.OAUTH_GOOGLE_CALLBACK_URL}&scope=https://www.googleapis.com/auth/userinfo.profile&response_type=code&state=${state}`
     }
 
     return urls
   }
 
-  public async githubLogin(code: string): Promise<{ token: string }> {
+  public async login(type: OAuthType, code: string): Promise<TokenResponse> {
     if (!code) {
-      throw new ApplicationError(
-        errorCode.AUTHENTICATION_ERROR,
-        'Github login failure.',
-      )
+      throw this.createAuthenticationError()
     }
 
-    try {
-      // Fetch Github Access Token
-      const { body: accessTokenResult } = await this.post<{
-        access_token: string
-      }>(`https://${env.OAUTH_GITHUB_DOMAIN}/login/oauth/access_token`, {
-        client_id: env.OAUTH_GITHUB_CLIENT_ID,
-        client_secret: env.OAUTH_GITHUB_CLIENT_SECRET,
-        code,
-      })
+    const profile = await this.fetchOAuthProfile(type, code)
+    const user = await this.findOrCreate(type, profile)
+    const token = getJWT(user)
 
-      const { body: profile } = await this.get<GithubProfile>(
-        'https://api.github.com/user',
-        {},
-        { Authorization: `Bearer ${accessTokenResult.access_token}` },
-      )
-
-      const user = await this.findOrCreate(
-        profile.id,
-        'github',
-        profile.name || profile.login || 'Anonymous',
-        profile.avatar_url || '',
-      )
-
-      const token = this.getJWT(user)
-
-      return { token }
-    } catch (error) {
-      console.error(error)
-      throw new ApplicationError(
-        errorCode.AUTHENTICATION_ERROR,
-        'Github login failure.',
-        { code },
-        error as Error,
-      )
-    }
+    return { token }
   }
 
-  public async googleLogin(code: string): Promise<{ token: string }> {
+  public async updateIcon(
+    userId: string,
+    type: OAuthType,
+    code: string,
+  ): Promise<TokenResponse> {
     if (!code) {
-      throw new ApplicationError(
-        errorCode.AUTHENTICATION_ERROR,
-        'Google login failure.',
-      )
+      throw this.createAuthenticationError()
     }
 
-    try {
-      // Fetch Google Access Token
-      const { body: accessTokenResult } = await this.post<{
-        access_token: string
-      }>(`https://accounts.google.com/o/oauth2/token`, {
-        client_id: env.OAUTH_GOOGLE_CLIENT_ID,
-        client_secret: env.OAUTH_GOOGLE_CLIENT_SECRET,
-        redirect_uri: env.OAUTH_GOOGLE_CALLBACK_URL,
-        grant_type: 'authorization_code',
-        code,
-      })
+    const user = await this.checkExistsUser(userId)
+    const profile = await this.fetchOAuthProfile(type, code)
 
-      const { body: profile } = await this.get<GoogleProfile>(
-        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessTokenResult.access_token}`,
-        {},
-      )
+    user.update({ icon: profile.icon })
+    this.userRepository.update(user)
 
-      if (profile.error) {
-        throw new ApplicationError(
-          errorCode.AUTHENTICATION_ERROR,
-          'Google login failure.',
-        )
-      }
+    const token = getJWT(user)
 
-      const user = await this.findOrCreate(
-        profile.id,
-        'google',
-        profile.name || 'Anonymous',
-        profile.picture || '',
-      )
+    return { token }
+  }
 
-      const token = this.getJWT(user)
-
-      return { token }
-    } catch (error) {
-      console.error(error)
-      throw new ApplicationError(
-        errorCode.AUTHENTICATION_ERROR,
-        'Google login failure.',
-        { code },
-        error as Error,
-      )
+  public async linkage(
+    userId: string,
+    type: OAuthType,
+    code: string,
+  ): Promise<TokenResponse> {
+    if (!code) {
+      throw this.createAuthenticationError()
     }
+
+    const user = await this.checkExistsUser(userId)
+    const profile = await this.fetchOAuthProfile(type, code)
+
+    const userCredential = new UserCredential({
+      userId,
+      oauthId: profile.id,
+      oauthType: type,
+    })
+
+    await this.userCredentialRepository.create(userCredential)
+
+    const token = getJWT(user)
+
+    return { token }
   }
 }
